ec15914f9c85c16c346c201f0f8930f3864cafde
LouisTsai <q1030176@gmail.com>
2024-06-18 15:24:48 +0800

    feat: complete loader implementation

--- src/main/java/ghidrevm/GhidrevmLoader.java
+++ src/main/java/ghidrevm/GhidrevmLoader.java
@@ -16,22 +16,32 @@
 package ghidrevm;
 
 import java.io.IOException;
+
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import ghidra.app.util.Option;
 import ghidra.app.util.bin.ByteProvider;
 import ghidra.app.util.importer.MessageLog;
-import ghidra.app.util.opinion.AbstractProgramWrapperLoader;
+//import ghidra.app.util.opinion.AbstractProgramWrapperLoader;
+import ghidra.app.util.opinion.AbstractLibrarySupportLoader;
 import ghidra.app.util.opinion.LoadSpec;
 import ghidra.framework.model.DomainObject;
 import ghidra.program.model.listing.Program;
+import ghidra.program.model.lang.LanguageCompilerSpecPair;
+import ghidra.program.model.address.Address;
+import ghidra.program.model.mem.MemoryBlock;
+import ghidra.program.flatapi.FlatProgramAPI;
 import ghidra.util.exception.CancelledException;
 import ghidra.util.task.TaskMonitor;
 
 /**
  * TODO: Provide class-level documentation that describes what this loader does.
  */
-public class GhidrevmLoader extends AbstractProgramWrapperLoader {
+public class GhidrevmLoader extends AbstractLibrarySupportLoader {
+	boolean fEVM = false; 	// EVM Hexadecimal
+	boolean fEVM_H = false; // EVM Byte Code 
 
 	@Override
 	public String getName() {
@@ -39,13 +49,21 @@ public class GhidrevmLoader extends AbstractProgramWrapperLoader {
 		// TODO: Name the loader.  This name must match the name of the loader in the .opinion 
 		// files.
 
-		return "My loader";
+		return "EVMLoader";
 	}
 
 	@Override
 	public Collection<LoadSpec> findSupportedLoadSpecs(ByteProvider provider) throws IOException {
 		List<LoadSpec> loadSpecs = new ArrayList<>();
-
+		this.fEVM = provider.getName().endsWith(".evm");
+		this.fEVM_H = provider.getName().endsWith(".evm_h");
+		
+		if(this.fEVM || this.fEVM_H) {
+			LanguageCompilerSpecPair compilerSpec = new LanguageCompilerSpecPair("evm:256:default", "default");
+			LoadSpec loadspec = new LoadSpec(this, 0, compilerSpec, true);
+			loadSpecs.add(loadspec);
+		}
+		
 		// TODO: Examine the bytes in 'provider' to determine if this loader can load it.  If it 
 		// can load it, return the appropriate load specifications.
 
@@ -58,6 +76,53 @@ public class GhidrevmLoader extends AbstractProgramWrapperLoader {
 			throws CancelledException, IOException {
 
 		// TODO: Load the bytes from 'provider' into the 'program'.
+		FlatProgramAPI flatAPI = new FlatProgramAPI(program);
+		
+		try {
+			if(this.fEVM) {
+				monitor.setMessage("EVM Code: Loading Starts");
+				Address addr = flatAPI.toAddr(0x0);
+				MemoryBlock block = flatAPI.createMemoryBlock("ram", addr, provider.readBytes(0, provider.length()), false);
+				
+				block.setRead(true);
+				block.setWrite(true);
+				block.setExecute(true);
+				
+				flatAPI.addEntryPoint(addr);
+				monitor.setMessage("EVM Code: Loading Ends");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new IOException("EVM Code: Loading Fails");
+		}
+		
+		try {
+			if(this.fEVM_H) {
+				monitor.setMessage("EVM Code: Loading Starts");
+				CharSequence seq = new String(provider.readBytes(0, provider.length()), "UTF-8");
+				Pattern p = Pattern.compile("[0-9a-fA-F]{2}");
+				Matcher m = p.matcher(seq);				
+				int count = m.groupCount();
+				
+				byte[] hex_code = new byte[count];
+				int i = 0;
+				while(m.find()) {
+					String digits = m.group();
+					hex_code[i++] = (byte)Integer.parseInt(digits, 16);
+				}
+				Address addr = flatAPI.toAddr(0x0);
+				MemoryBlock block = flatAPI.createMemoryBlock("ram", addr, hex_code, false);
+				
+				block.setRead(true);
+				block.setWrite(true);
+				block.setExecute(true);
+				
+				flatAPI.addEntryPoint(addr);
+				monitor.setMessage("EVM Code: Loading Ends");
+			}
+		} catch(Exception e) {
+			throw new IOException("EVM Code: Loading Ends");
+		}
 	}
 
 	@Override
@@ -67,7 +132,7 @@ public class GhidrevmLoader extends AbstractProgramWrapperLoader {
 			super.getDefaultOptions(provider, loadSpec, domainObject, isLoadIntoProgram);
 
 		// TODO: If this loader has custom options, add them to 'list'
-		list.add(new Option("Option name goes here", "Default option value goes here"));
+		// list.add(new Option("Option name goes here", "Default option value goes here"));
 
 		return list;
 	}
